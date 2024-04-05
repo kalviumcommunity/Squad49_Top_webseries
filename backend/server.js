c15a640f6ff91e45f41af32940914a48e122d65b
ishant1800 <ishanaryan45@gmail.com>
2024-04-05 17:54:22 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -3,102 +3,124 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+require('dotenv').config();
 
 const app = express();
 const PORT = 3000;
+const routes = require('./routes');
 
 app.use(bodyParser.json());
-
-const url = 'mongodb://localhost:27017';
-const dbName = 'WebSeries'; 
+app.use(cookieParser());
+app.use('/', routes);
 
 // Defining Mongoose Schema
 const userSchema = new mongoose.Schema({
-    username: { type: String, required: true },
-    password: { type: String, required: true }
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  seriesData: { type: Object, required: true },
 });
 
-// Creating Mongoose Model
 const User = mongoose.model('User', userSchema);
 
-// Connecting to MongoDB using Mongoose
-mongoose.connect(`${url}/${dbName}`)
-    .then(() => {
-        console.log('Connected to MongoDB');
-        // Start server
-        app.listen(PORT, () => {
-            console.log(`🚀 Server running on PORT: ${PORT}`);
-        });
-    })
-    .catch(err => {
-        console.error('Error connecting to MongoDB:', err);
+// Connecting  MongoDB using Mongoose
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log('Connected to MongoDB');
+
+    app.listen(PORT, () => {
+      console.log(`🚀 Server running on PORT: ${PORT}`);
     });
+  })
+  .catch(err => {
+    console.error('Error connecting to MongoDB:', err);
+  });
 
-// Endpoint to register a new user
-app.post('/register', async (req, res) => {
-    try {
-        const { username, password } = req.body;
-
-        // Hash the password before saving it to the database
-        const hashedPassword = await bcrypt.hash(password, 10);
-
-        // Create new user using Mongoose model
-        const user = new User({ username, password: hashedPassword });
-        await user.save();
-
-        res.status(201).json({ message: 'User registered successfully' });
-    } catch (error) {
-        console.error('Error registering user:', error);
-        res.status(500).json({ error: 'Internal server error' });
-    }
-});
-
-// Endpoint to authenticate and login a user
-app.post('/login', async (req, res) => {
-    try {
-        const { username, password } = req.body;
-
-        // Find the user by username
-        const user = await User.findOne({ username });
-
-        if (!user) {
-            return res.status(401).json({ error: 'Invalid username or password' });
-        }
-
-        
-        const passwordMatch = await bcrypt.compare(password, user.password);
-
-        if (!passwordMatch) {
-            return res.status(401).json({ error: 'Invalid username or password' });
-        }
-
-        const token = jwt.sign({ username: user.username }, 'your_secret_key', { expiresIn: '1h' });
-
-        res.status(200).json({ token });
-    } catch (error) {
-        console.error('Error logging in:', error);
-        res.status(500).json({ error: 'Internal server error' });
-    }
-});
-
-
+// Middleware to authenticate token
 const authenticateToken = (req, res, next) => {
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1];
+  const token = req.cookies.token;
 
-    if (!token) {
-        return res.status(401).json({ error: 'Unauthorized' });
+  if (!token) {
+    return res.status(401).json({ error: 'Unauthorized' });
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) {
+      return res.status(403).json({ error: 'Forbidden' });
     }
-
-    jwt.verify(token, 'your_secret_key', (err, user) => {
-        if (err) {
-            return res.status(403).json({ error: 'Forbidden' });
-        }
-        req.user = user;
-        next();
-    });
+    req.user = user;
+    next();
+  });
 };
 
+app.get('/', (req, res) => {
+  res.send(`Database Connection Status: ${mongoose.connection.readyState}`);
+});
+
+app.post('/register', (req, res) => {
+  const { username, password, seriesData } = req.body;
+
+  User.findOne({ username })
+    .then(user => {
+      if (user) {
+        return res.status(400).json({ error: 'User already exists' });
+      }
+
+      const newUser = new User({
+        username,
+        password: bcrypt.hashSync(password, 10),
+        seriesData,
+      });
+
+      return newUser.save();
+    })
+    .then(user => {
+      res.status(201).json({ message: 'User registered successfully', user });
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ error: 'Internal server error' });
+    });
+});
+
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+
+  User.findOne({ username })
+    .then(user => {
+      if (!user) {
+        return res.status(400).json({ error: 'Invalid credentials' });
+      }
+
+      if (!bcrypt.compareSync(password, user.password)) {
+        return res.status(400).json({ error: 'Invalid credentials' });
+      }
+
+      const token = jwt.sign(
+        {
+          username: user.username,
+          seriesData: user.seriesData,
+        },
+        process.env.JWT_SECRET,
+        {
+          expiresIn: '1h',
+        },
+      );
+
+      // Setting token in cookie
+      res.cookie('token', token, { httpOnly: true });
+
+      res.status(200).json({ message: 'Logged in successfully', token });
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ error: 'Internal server error' });
+    });
+});
+
 app.get('/protected', authenticateToken, (req, res) => {
-    res.json(req.user);
+  res.json({
+    username: req.user.username,
+    seriesData: req.user.seriesData,
+  });
 });
